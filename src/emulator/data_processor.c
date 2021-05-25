41edfef974f33dfeb5881e9c95d5ae73f9b603fc
Athena Yim <athenayimws@gmail.com>
2021-05-25 01:39:23 +0100

    Implement all base operations without CPSR flag setting

--- src/emulator/data_processor.c
+++ src/emulator/data_processor.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include "functions.h"
 
+// TODO: check 'Set Condition Codes' bit and set CPSR flags accordingly
+
 // Performs right rotation on a 32-bit binary number
 int rotate_right(uint32_t value, unsigned int rotation) {
 	unsigned int shifted = value >> rotation;
@@ -10,13 +12,61 @@ int rotate_right(uint32_t value, unsigned int rotation) {
 }
 
 // Bitwise AND
-void and(Register *rd, Register *rn, uint32_t operand2) {
+uint32_t and(Register *rd, Register *rn, uint32_t operand2) {
 	*rd = *rn & operand2;
+	return *rd;
 }
 
 // Bitwise exclusive OR
-void eor(Register *rd, Register *rn, uint32_t operand2) {
+uint32_t eor(Register *rd, Register *rn, uint32_t operand2) {
 	*rd = (*rn & ~operand2) | (~*rn & operand2);
+	return *rd;
+}
+
+// Subtract operand2 from register
+uint32_t sub(Register *rd, Register *rn, uint32_t operand2) {
+	*rd = *rn - operand2;
+	return *rd;
+}
+
+// Subtract register from operand2
+uint32_t rsb(Register *rd, Register *rn, uint32_t operand2) {
+	*rd = operand2 - *rn;
+	return *rd;
+}
+
+
+// Addition
+uint32_t add(Register *rd, Register *rn, uint32_t operand2) {
+	*rd = *rn + operand2;
+	return *rd;
+}
+
+// Bitwise AND, result not written
+uint32_t tst(Register *rn, uint32_t operand2) {
+	return *rn & operand2;
+}
+
+// Bitwise XOR, result not written
+uint32_t teq(Register *rn, uint32_t operand2) {
+	return (*rn & ~operand2) | (~*rn & operand2);
+}
+
+// sub, result not written
+uint32_t cmp(Register *rn, uint32_t operand2) {
+	return *rn - operand2;
+}
+
+// Bitwise OR
+uint32_t orr(Register *rd, Register *rn, uint32_t operand2) {
+	*rd = *rn | operand2;
+	return *rd;
+}
+
+// Move operand2 to dest
+uint32_t mov(Register *rd, uint32_t operand2) {
+	*rd = operand2;
+	return *rd;
 }
 
 int main(void) {
