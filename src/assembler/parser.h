e16920993b68e92631aec943e58e8da69945e8ed
tg1120 <tg1120@ic.ac.uk>
2021-06-08 17:19:09 +0100

    added parser function explanations

--- src/assembler/parser.h
+++ src/assembler/parser.h
@@ -2,15 +2,35 @@
 #define PARSER_H
 #include "datatypes.h"
 
-mnemonic_t string_to_mnemonic(char *str);
-condition_t string_to_condition(char *str);
-opcode_t mnemonic_to_opcode(mnemonic_t mnemonic);
-reg_address_t string_to_reg_address(char *str);
-shift_t string_to_shift(char *str);
 
-void parse_shift(string_array_t *tokens, Instruction *instruction);
-void parse_operand(string_array_t *tokens, Instruction *instruction);
+//Returns mnemonic for a given string
+Mnemonic string_to_mnemonic(char *str);
 
-word_t parse_immediate_value(char *str);
+
+//Returns the condition for a given string
+Condition string_to_condition(char *str);
+
+
+//Returns opcode for a given mnemonic
+Opcode mnemonic_to_opcode(Mnemonic mnemonic);
+
+
+//Returns register address (number) given by the string
+Reg_address string_to_reg_address(char *str);
+
+
+//Returns the shift for a given shift string
+Shift string_to_shift(char *str);
+
+//Parses the shift and adds it to the given instruction
+void parse_shift(String_array *tokens, Instruction *instruction);
+
+
+//Parses operand and adds it to the given instruction
+void parse_operand(String_array *tokens, Instruction *instruction);
+
+
+//Returns the immediate value given by the string
+Word parse_immediate_value(char *str);
 
 #endif
