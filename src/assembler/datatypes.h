f867f06a3fb8fc4c5c336097e0d4850d3eb8aea4
Lee Wan Sze <wsl20@doc.ic.ac.uk>
2021-06-08 14:38:43 +0100

    Added an Enum to represent the Mnemonics of different operations

--- src/assembler/datatypes.h
+++ src/assembler/datatypes.h
@@ -4,4 +4,33 @@
 
 typedef uint32_t Instruction;
 
+
+/* Enum for representing the Mnemonics of different operations */
+typedef enum {
+    ADD,
+    SUB,
+    RSB,
+    AND,
+    EOR,
+    ORR,
+    MOV,
+    TST,
+    TEQ,
+    CMP,
+    MUL,
+    MLA,
+    LDR,
+    STR,
+    BEQ,
+    BNE,
+    BGE,
+    BLT,
+    BGT,
+    BLE,
+    B,
+    LSL,
+    ANDEQ
+} Operation;
+
+
 #endif
