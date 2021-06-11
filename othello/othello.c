f65e74190338809b5cb58db1bc3240414f34abb5
Lee Wan Sze <wsl20@doc.ic.ac.uk>
2021-06-11 14:14:38 +0100

    Implemented more functions from othello.h in othello.c

--- othello/othello.c
+++ othello/othello.c
@@ -16,6 +16,12 @@ board_t* initial_board(void) {
         }
     }    
 
+    *board[3][3] = 'O';
+    *board[3][4] = 'X';
+    *board[4][3] = 'X';
+    *board[4][4] = 'O';
+
+
     return board;
 }
 
@@ -23,17 +29,119 @@ board_t* initial_board(void) {
 void print_board(board_t* board) {
     for(int row = 0; row < HEIGHT; row ++) {
         for(int col = 0; col < WIDTH; col ++) {
-
             if(col == 7) {
                 printf(" %c ", *board[row][col]);
             } else {
                 printf(" %c |", *board[row][col]);
             }
         }
+
         printf("\n");
     }
 };
 
+// determines if the move is legal 
+// (ie. if move if within bounds, and the position is empty)
+// (and if there is another piece of the same type horizontally, vertically or diagonally)
+bool is_legal(move_t move, board_t* board){
+    // check if move is within bounds
+    if(move.row < 0 || move.row > 7 || move.col < 0 || move.col > 7){
+        return false;
+    }
+
+    // check if position is empty
+    if (*board[move.row][move.col] != '-'){
+        return false;
+    }
+
+    char player_piece;
+
+    if(move.player == BLACK){
+        player_piece = 'X';
+    } else if (move.player == WHITE){
+        player_piece = 'O';
+    }
+    
+    // check if there is another piece of same type vertically
+    for(int r = 0; r < HEIGHT; r ++){
+        if(*board[r][move.col] == player_piece){
+            return true;
+        }
+    }
+
+    // check if there is another piece of same type horitzontally
+    for(int c = 0; c < WIDTH; c ++){
+        if(*board[move.row][c] == player_piece){
+            return true;
+        }
+    }
+
+    // check if there is another piece of same type diagonally
+    for(int r = 0; r < HEIGHT; r ++) {
+        for(int c = 0; c < WIDTH; c ++) {
+            if(move.row + r < 8){
+                if(move.col + c < 8) {
+                    if(*board[move.row + r][move.col + c] == player_piece) {
+                        return true;
+                    }
+                }
+            }
+
+            if(move.row - r < 8){
+                if(move.col - c < 8) {
+                    if(*board[move.row + r][move.col + c] == player_piece) {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    return true;
+}
+
+
+// checks if game has ended (i.e. if all spaces are either an X or O)
+bool endgame(board_t* board){
+    for(int row = 0; row < HEIGHT; row ++) {
+        for(int col = 0; col < WIDTH; col ++) {
+            if (*board[row][col] == '-') {
+                return false;
+            };
+        }
+    }   
+    return true;
+}
+
+
+// determines the winning player or if it is a draw
+int outcome(board_t* board){
+    // player 1 == X and player 2 == O
+    int player1 = 0;
+    int player2 = 0;
+
+    for(int row = 0; row < HEIGHT; row ++) {
+        for(int col = 0; col < WIDTH; col ++) {
+            if (*board[row][col] == 'X') {
+                player1 ++;
+            } else if (*board[row][col] == 'O') {
+                player2 ++;
+            }
+        }
+    }   
+
+    if (player1 > player2){
+        return 1;
+    } if (player1 < player2){
+        return 2;
+    }
+
+    return 0;
+}
+
+// temporary main function for testing
 int main(void){
     board_t* board = initial_board();
     print_board(board);
--- othello/othello.h
+++ othello/othello.h
@@ -1,16 +1,20 @@
 #ifndef __OTHELLO_H__
 #define __OTHELLO_H__
-
 #include <stdbool.h>
+
 #define HEIGHT 8
 #define WIDTH 8
 
+// Player1 = X and Player2 = O
+
 typedef char board_t[HEIGHT][WIDTH];
+
+typedef enum Player {BLACK = 1, WHITE = 2} Player;
 
 typedef struct {
 	int row;
 	int col;
-	int player;
+	Player player;
 } move_t;
 
 typedef struct {
@@ -33,11 +37,11 @@ void make_move(move_t move, board_t* board);
 // determines an array of legal moves
 legalmoves_t* legalmoves(board_t* board);
 
-// determines if this is a full board 
-bool is_full_board(board_t* board);
-
-// determines the winning player or if it is a draw
+// returns 1 if player1 (X) wins, 2 if player2 (O) wins, or 0 otherwise
 int outcome(board_t* board);
+
+// checks if game has ended (i.e. if all spaces are either an X or O)
+bool endgame(board_t* board);
 
 #endif
 
