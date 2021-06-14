963048ac019da035b90526ff64b601e9b3ee63a0
Lee Wan Sze <wsl20@doc.ic.ac.uk>
2021-06-14 15:49:53 +0100

    implementation for random bot done

--- othello/playerbot.c
+++ othello/playerbot.c
@@ -10,8 +10,15 @@ void greedy(board_t board, move_t* move, Player player, legalmoves_t* legalmoves
 
 // random implementation
 void randomize(board_t board, move_t* move, Player player, legalmoves_t* legalmoves) {
-    // TODO
+    // Initializes random number generator
+    srand(time(NULL));  
+    
+    // Pick a random number legalmove from the array and store it in the move ptr
+    int index = rand() % sizeof(legalmoves -> moves);
+    *move = legalmoves -> moves[index];
 }
+
+
 
 // minimax implementation
 void minimax(board_t board, move_t* move, Player player, legalmoves_t* legalmoves) {
