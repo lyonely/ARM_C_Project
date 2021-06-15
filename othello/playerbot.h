470c8d2c004cde4668e858edf74039bd85747c32
tg1120 <tg1120@ic.ac.uk>
2021-06-15 13:51:00 +0100

    turbo tony

--- othello/playerbot.h
+++ othello/playerbot.h
@@ -11,6 +11,9 @@ void randomize(board_t board, move_t* move, Player player, legalmoves_t* legalmo
 // minimax implementation
 void minimax(board_t board, move_t* move, Player player, legalmoves_t* legalmoves);
 
+// minimax_1 implementation
+void minimax_1(board_t board, move_t* move, Player player, legalmoves_t* legalmoves); 
+
 //lousy implementation
 void lousy(board_t board, move_t* moev, Player player, legalmoves_t* legalmoves);
 
